5712823c199f07761942aaf41d96fb410fa9f5a0
vivek khade <khadev881@gmail.com>
2022-09-22 18:47:44 +0530

    resume issue  fixed

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/vivekkhade23/vivekkhade23.github.io/raw/master/src/Assets/VivekResume.pdf"
+  "https://github.com/vivekkhade23/vivekkhade23.github.io/raw/master/src/Assets/Vivek-Khade-Resume.pdf"
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
